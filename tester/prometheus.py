673d7eb0981900a574e2742d6e0733859b813385
Balbir Thomas <balbir.thomas@canonical.com>
2021-05-05 11:11:30 +0100

    Created rudimentary test data generator
    This commit adds a very basic prometheus test data generator
    utility, packaged into a docker file. This docker images is
    the OCI image resource used by this charm.

---
+++ tester/prometheus.py
@@ -0,0 +1,31 @@
+import random
+import time
+
+from prometheus_client import start_http_server, Summary
+
+
+# Metric that tracks time spent and number of requests made.
+REQUEST_TIME = Summary('request_processing_seconds',
+                       'Time spent processing request')
+
+
+@REQUEST_TIME.time()
+def process_request(t):
+    """A fake function that takes a configurable amount of time.
+
+    Args:
+        t: integer specifying amount of time that should be
+           spent in processing this request
+    """
+    time.sleep(t)
+
+
+def main(port=8000):
+    """Expose a metrics endpoint to prometheus"""
+    start_http_server(port)
+    while True:
+        process_request(random.random())
+
+
+if __name__ == '__main__':
+    main()
