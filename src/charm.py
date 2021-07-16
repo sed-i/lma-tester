cfb86af8a3c4f26a0d8ae5e639c70865b88daf19
Balbir Thomas <balbir.thomas@canonical.com>
2021-07-16 10:38:33 +0100

    Updated to multi job config prototype

--- src/charm.py
+++ src/charm.py
@@ -24,8 +24,19 @@ class PrometheusTesterCharm(CharmBase):
     def __init__(self, *args):
         super().__init__(*args)
         self._stored.set_default(monitoring_enabled=False)
-        scrape_config = {"static_scrape_port": 8000}
-        self.prometheus = PrometheusConsumer(self, "monitoring", self._consumes, "prometheus_tester", config=scrape_config)
+        jobs = [
+            {
+                "static_configs": [
+                    {
+                        "targets": ["*:8000"],
+                        "labels": {
+                            "status": "testing"
+                        }
+                    }
+                ]
+            }
+        ]
+        self.prometheus = PrometheusConsumer(self, "monitoring", self._consumes, "prometheus_tester", jobs=jobs)
         self.framework.observe(self.on.prometheus_tester_pebble_ready,
                                self._on_prometheus_tester_pebble_ready)
         self.framework.observe(self.on.config_changed, self._on_config_changed)
