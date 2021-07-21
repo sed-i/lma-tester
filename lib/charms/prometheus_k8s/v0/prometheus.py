ef7b4641d32127d9a9e68674a22a2955fac8fdd4
Balbir Thomas <balbir.thomas@canonical.com>
2021-07-21 12:16:21 +0100

    Updated library with metrics path bug fix

--- lib/charms/prometheus_k8s/v0/prometheus.py
+++ lib/charms/prometheus_k8s/v0/prometheus.py
@@ -268,7 +268,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 1
+LIBPATCH = 2
 
 logger = logging.getLogger(__name__)
 
@@ -276,8 +276,8 @@ logger = logging.getLogger(__name__)
 def _sanitize_scrape_configuration(job):
     return {
         "job_name": job.get("job_name"),
-        "metrics_path": job.get("metrics_path"),
-        "static_configs": job.get("static_configs"),
+        "metrics_path": job.get("metrics_path", "/metrics"),
+        "static_configs": job.get("static_configs", [{"targets": ["*:80"]}]),
     }
 
 
@@ -463,9 +463,9 @@ class PrometheusProvider(ProviderBase):
             for a single job.
         """
         name = job.get("job_name")
-        job_name = "job_name_prefix_{}".format(name) if name else job_name_prefix
+        job_name = "{}_{}".format(job_name_prefix, name) if name else job_name_prefix
 
-        config = {"job_name": job_name}
+        config = {"job_name": job_name, "metrics_path": job["metrics_path"]}
 
         static_configs = job.get("static_configs")
         config["static_configs"] = []
